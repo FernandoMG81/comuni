4c5254f7e3e1744fe3b75358ba6285cfdd317dc3
Javier <angelellij@users.noreply.github.com>
2019-09-02 14:03:58 -0300

    Clases Basicas Listas
    Falto nomas poner algunos datos de mensajes. Igual me gustaria ver si sirve como estan puestos antes de seguir escribiendo.

---
+++ app/src/main/java/com/example/comuni/Models/Mensaje.java
@@ -0,0 +1,47 @@
+package com.example.comuni.Models;
+
+public class Mensaje {
+    private int id;
+    private int postId;
+    private int UsuarioId;
+    private String mensaje;
+
+    public Mensaje(int id, int postId, int usuarioId, String mensaje) {
+        this.id = id;
+        this.postId = postId;
+        UsuarioId = usuarioId;
+        this.mensaje = mensaje;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public int getPostId() {
+        return postId;
+    }
+
+    public void setPostId(int postId) {
+        this.postId = postId;
+    }
+
+    public int getUsuarioId() {
+        return UsuarioId;
+    }
+
+    public void setUsuarioId(int usuarioId) {
+        UsuarioId = usuarioId;
+    }
+
+    public String getMensaje() {
+        return mensaje;
+    }
+
+    public void setMensaje(String mensaje) {
+        this.mensaje = mensaje;
+    }
+}
--- app/src/main/java/com/example/comuni/Models/Post.java
+++ app/src/main/java/com/example/comuni/Models/Post.java
@@ -1,8 +1,108 @@
 package com.example.comuni.Models;
 
+import java.util.Date;
+
 public class Post {
     private int id = -1;
     private int espacioId = -1;
     private int usuarioId = -1;
+    private String titulo;
+    private String mensaje;
+    private int upvotes;
+    private boolean importante;
 
+    // Para despues
+    private Date creado = null;
+    private Date modificado = null;
+    private Date eliminado = null;
+
+    public Post(int id, int espacioId, int usuarioId, String titulo, String mensaje, int upvotes, boolean importante) {
+        this.id = id;
+        this.espacioId = espacioId;
+        this.usuarioId = usuarioId;
+        this.titulo = titulo;
+        this.mensaje = mensaje;
+        this.upvotes = upvotes;
+        this.importante = importante;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public int getEspacioId() {
+        return espacioId;
+    }
+
+    public void setEspacioId(int espacioId) {
+        this.espacioId = espacioId;
+    }
+
+    public int getUsuarioId() {
+        return usuarioId;
+    }
+
+    public void setUsuarioId(int usuarioId) {
+        this.usuarioId = usuarioId;
+    }
+
+    public String getTitulo() {
+        return titulo;
+    }
+
+    public void setTitulo(String titulo) {
+        this.titulo = titulo;
+    }
+
+    public String getMensaje() {
+        return mensaje;
+    }
+
+    public void setMensaje(String mensaje) {
+        this.mensaje = mensaje;
+    }
+
+    public int getUpvotes() {
+        return upvotes;
+    }
+
+    public void setUpvotes(int upvotes) {
+        this.upvotes = upvotes;
+    }
+
+    public boolean isImportante() {
+        return importante;
+    }
+
+    public void setImportante(boolean importante) {
+        this.importante = importante;
+    }
+
+    public Date getCreado() {
+        return creado;
+    }
+
+    public void setCreado(Date creado) {
+        this.creado = creado;
+    }
+
+    public Date getModificado() {
+        return modificado;
+    }
+
+    public void setModificado(Date modificado) {
+        this.modificado = modificado;
+    }
+
+    public Date getEliminado() {
+        return eliminado;
+    }
+
+    public void setEliminado(Date eliminado) {
+        this.eliminado = eliminado;
+    }
 }
