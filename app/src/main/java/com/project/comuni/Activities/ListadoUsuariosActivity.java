4ea106acc7a4a71e1f5dba337a810a1db30fcf3e
Fernando Martin G <fermg299@outlook.com>
2019-09-19 07:57:37 -0300

    Agregado opciones en la barra de menu

--- app/src/main/java/com/project/comuni/Activities/ListadoUsuariosActivity.java
+++ app/src/main/java/com/project/comuni/Activities/ListadoUsuariosActivity.java
@@ -13,7 +13,7 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.LinearLayout;
 import android.widget.TextView;
-import android.widget.Toast;
+
 
 import com.bumptech.glide.Glide;
 import com.firebase.ui.database.FirebaseRecyclerAdapter;
