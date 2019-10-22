05fa8e6bbd4eb090989e5d0492c382b4f433123b
Javier <angelellij@gmail.com>
2019-10-22 13:43:51 -0300

    Messages con search

--- app/src/main/java/com/project/comuni/Fragments/MessagesFragment.java
+++ app/src/main/java/com/project/comuni/Fragments/MessagesFragment.java
@@ -2,10 +2,13 @@ package com.project.comuni.Fragments;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
 
 import com.bumptech.glide.Glide;
@@ -31,23 +34,72 @@ import de.hdodenhof.circleimageview.CircleImageView;
 
 import java.util.ArrayList;
 
+import static com.project.comuni.Utils.Util.filtrarString;
+
 public class MessagesFragment extends Fragment {
-    private UsuarioService usuarioService = new UsuarioService();
-    private MensajeService mensajeService = new MensajeService();
+    //Variables Locales
+    String searchText = "";
 
-
+    //Variables Datos
     private ArrayList<Mensaje> mensajes = new ArrayList<>();
-    @Nullable
-    @Override
-    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+    private ArrayList<Mensaje> mensajesAMostrar = new ArrayList<>();
 
-        View view = inflater.inflate(R.layout.fragment_messages, container, false);
+    //Layout
+    private Button botonNuevoMensaje;
+    private RecyclerView recyclerView;
+    private EditText search;
 
-        RecyclerView recyclerView = view.findViewById(R.id.RVMesages);
-        Button botonNuevoMensaje = view.findViewById(R.id.addBtnMensaje);
+    private void getdata(){
+        MensajeService mensajeService = new MensajeService();
+        mensajes = mensajeService.filterMensajesPorNVueltas(4);
+    }
 
+    private void setLayoutReferences(View view){
+        recyclerView = view.findViewById(R.id.RVMesages);
+        botonNuevoMensaje = view.findViewById(R.id.addBtnMensaje);
+        search = view.findViewById(R.id.MessagesSearch);
+    }
 
+    private void setSearch(){
+        search.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence charSequence, int i, int i1, int i2) {
 
+            }
+
+            @Override
+            public void onTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+
+            }
+
+            @Override
+            public void afterTextChanged(Editable editable) {
+                searchText = editable.toString();
+                filterData();
+                setRecyclerMensajes();
+            }
+        });
+    }
+
+    private void filterData(){
+        mensajesAMostrar.clear();
+        for (Mensaje mensaje : mensajes){
+            if (filtrarString(mensaje.getTexto(),searchText) ||
+                    filtrarString(mensaje.getEmisor().getNombre() + " " + mensaje.getEmisor().getApellido(), searchText) ||
+                    filtrarString(mensaje.getReceptor().getNombre() + " " + mensaje.getReceptor().getApellido(), searchText)
+            ){
+              mensajesAMostrar.add(mensaje);
+            }
+        }
+    }
+
+    private void setRecyclerMensajes(){
+        recyclerView.setLayoutManager(new LinearLayoutManager(getContext()));
+        RecyclerAdapterMessages adapter = new RecyclerAdapterMessages(mensajesAMostrar, this.getContext());
+        recyclerView.setAdapter(adapter);
+    }
+
+    private void addButton(){
         botonNuevoMensaje.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -55,15 +107,21 @@ public class MessagesFragment extends Fragment {
                 startActivity(intent);
             }
         });
-        recyclerView.setLayoutManager(new LinearLayoutManager(getContext()));
+    }
 
-        mensajes = mensajeService.filterMensajesPorNVueltas(4);
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_messages, container, false);
 
-        RecyclerAdapterMessages adapter = new RecyclerAdapterMessages(mensajes, this.getContext());
+        getdata();
+        setLayoutReferences(view);
+        setSearch();
+        filterData();
+        setRecyclerMensajes();
+        addButton();
 
-        recyclerView.setAdapter(adapter);
-
-       return view;
+        return view;
     }
 
 
