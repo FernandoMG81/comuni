5a77f8ab6f7d6d767905b2f25717d575e2fabfb5
Fernando Martin G <fermg299@outlook.com>
2019-11-16 13:11:00 -0300

    pruebas splash screen

--- app/src/main/java/com/project/comuni/Activities/SplashActivity.java
+++ app/src/main/java/com/project/comuni/Activities/SplashActivity.java
@@ -7,15 +7,27 @@ import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.text.TextUtils;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.ImageView;
 
+import com.project.comuni.R;
 import com.project.comuni.Utils.Util;
 
 public class SplashActivity extends AppCompatActivity {
 
     private SharedPreferences prefs;
+    private Animation anim;
+    private ImageView imagen;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        //imagen = findViewById(R.id.);
+        anim = AnimationUtils.loadAnimation(this, R.anim.alpha);
+        anim.setDuration(2000);
+        anim.setRepeatMode(Animation.REVERSE);
+        anim.setRepeatCount(Animation.INFINITE);
 
         prefs = getSharedPreferences("PreferencesComuni", Context.MODE_PRIVATE);
 
