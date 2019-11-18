62a93dc2cf844fd7c96d1ba1aced0c123018f97a
Fernando Gordillo <fermg299@outlook.com>
2019-11-17 23:21:49 -0600

    Login ProgressBar

--- app/src/main/java/com/project/comuni/Activities/LoginActivity.java
+++ app/src/main/java/com/project/comuni/Activities/LoginActivity.java
@@ -5,12 +5,11 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
-import android.provider.ContactsContract;
 import android.text.TextUtils;
-import android.util.Log;
 import android.util.Patterns;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ProgressBar;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -20,16 +19,9 @@ import com.google.android.gms.tasks.Task;
 import com.google.android.material.textfield.TextInputEditText;
 import com.google.firebase.FirebaseApp;
 import com.google.firebase.auth.AuthResult;
-import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
-import com.google.firebase.database.DatabaseReference;
-import com.google.firebase.database.FirebaseDatabase;
-import com.project.comuni.Models.Firebase.Go;
-import com.project.comuni.Models.Usuario;
 import com.project.comuni.R;
-import com.project.comuni.Servicios.Db;
 import com.project.comuni.Servicios.LoginService;
-import com.project.comuni.Servicios.UsuarioService;
 
 
 public class LoginActivity extends AppCompatActivity {
@@ -39,6 +31,7 @@ public class LoginActivity extends AppCompatActivity {
     private TextInputEditText editTextPassword;
     private Button btnLogin;
     private TextView btnRegistro;
+    private ProgressBar progressBar;
 
     //Variables
     private String email;
@@ -51,9 +44,12 @@ public class LoginActivity extends AppCompatActivity {
 
     private void setLayout (){
         editTextLogin = findViewById(R.id.editTextUsuario);
-        editTextPassword = findViewById(R.id.editTextClave);
-        btnLogin = findViewById(R.id.buttonIngresar);
-        btnRegistro = findViewById(R.id.buttonRegistrarse);
+        editTextPassword = findViewById(R.id.loginClave);
+        btnLogin = findViewById(R.id.LoginButtonIngresar);
+        btnRegistro = findViewById(R.id.loginButtonRegistrarse);
+        progressBar = findViewById(R.id.loginProgressbar);
+        progressBar.setVisibility(View.INVISIBLE);
+        btnLogin.setEnabled(true);
     }
 
     private void setBtnLoginClick(){
@@ -63,14 +59,20 @@ public class LoginActivity extends AppCompatActivity {
             public void onClick(View v) {
                 email = editTextLogin.getText().toString();
                 contrasena = editTextPassword.getText().toString();
+                progressBar.setVisibility(View.VISIBLE);
+                btnLogin.setEnabled(false);
                 if (!(validarEmail(email) && validarContrasena(contrasena))) {
                     Toast.makeText(LoginActivity.this, "Contraseña y/o email no válidos", Toast.LENGTH_LONG).show();
+                    progressBar.setVisibility(View.INVISIBLE);
+                    btnLogin.setEnabled(true);
                 } else {
                     new LoginService().SignIn(email, contrasena).addOnCompleteListener(LoginActivity.this, new OnCompleteListener<AuthResult>() {
                         @Override
                         public void onComplete(@NonNull Task<AuthResult> task) {
                             if (!task.isSuccessful()) {
-                                Toast.makeText(LoginActivity.this, "Contraseña y/o email incorrectos", Toast.LENGTH_LONG).show();
+                                Toast.makeText(LoginActivity.this, task.getException().toString(), Toast.LENGTH_LONG).show();
+                                progressBar.setVisibility(View.INVISIBLE);
+                                btnLogin.setEnabled(true);
                             } else {
                                 Toast.makeText(LoginActivity.this, "Se logueo correctamente", Toast.LENGTH_LONG).show();
                                 nextActivity();
